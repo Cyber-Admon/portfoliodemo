d6595c1effc441a8c71756b20d0bdfbc7c2c1e31
Omoniyi Abdulmalik Adebayo (Admon) <pdfrinceadebayo123@gmail.com>
2025-07-07 00:31:39 +0100

    nav commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,17 @@ import "./index.css";
 
 const App = () => {
   const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <>
       {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
-      <div className={`min-h-screen transition-opacity duration-700 ${isLoaded? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
-        <Navbar />
+      <div
+        className={`min-h-screen transition-opacity duration-700 ${
+          isLoaded ? "opacity-100" : "opacity-0"
+        } bg-black text-gray-100`}
+      >
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       </div>
     </>
   );
