98f4773fe4d0e8d8f150308d3ae88cfbdd30d73a
Omoniyi Abdulmalik Adebayo (Admon) <pdfrinceadebayo123@gmail.com>
2025-07-07 00:09:18 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import LoadingScreen from "./components/LoadingScreen";
+import Navbar from "./components/Navbar";
+import "./index.css";
+
+const App = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
+  return (
+    <>
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
+      <div className={`min-h-screen transition-opacity duration-700 ${isLoaded? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
+        <Navbar />
+      </div>
+    </>
+  );
+};
+
+export default App;
